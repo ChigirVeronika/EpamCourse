5e90812d59d2374e1c5b766f37bf4ec77ffda09e
ViolaWittrockiana <chigir.vera@mail.ru>
2016-01-08 21:17:59 +0300

    files

--- MultiFiles/src/by/epam/files/main/Main.java
+++ MultiFiles/src/by/epam/files/main/Main.java
@@ -1,10 +1,21 @@
 package by.epam.files.main;
 
+import java.util.Scanner;
+
 /**
- * Created by Вероника on 02.01.2016.
+ * Created by Вероника on 08.01.2016.
  */
 public class Main {
     public static void main(String[] args){
+        //потом я это буду комментить, т.к. с этого элемента буду считывать путь
+        args[0]="E:\\EpamCourse\\MultiFiles\\src\\by\\epam\\files\\resource\\";
+
+        if(args[0]!=""){
+            
+        }
+        else{
+            System.out.println("Empty string.");
+        }
 
     }
 }
