20344e84ad4ab5569720358042471e40401cbba9
ViolaWittrockiana <chigir.vera@mail.ru>
2016-01-27 02:15:14 +0300

    socket

--- WebSocket/src/by/epam/websocket/service/HttpResponseParser.java
+++ WebSocket/src/by/epam/websocket/service/HttpResponseParser.java
@@ -8,8 +8,12 @@ import java.util.List;
  * Parse header of http response.
  */
 public class HttpResponseParser {
-    private static final String EMPTY_STRING = "";
+    private static final String STATUS_CODE = "(\\s{1}\\d+)";
+    private static final String TEXT_CODE = "([a-zA-Z&&[^HTTP]])";
+    
+
     private static final String NEXT_LINE="\n";
+    private static final String EMPTY_SIGN="\r";
     private static final String HTTP ="HTTP";
     private static final String SPACE =" ";
 
@@ -24,30 +28,37 @@ public class HttpResponseParser {
         do {//try to find empty string - means border between header and body
             headerList.add(list.get(j));
             j++;
-        }while (!list.get(j).equals(EMPTY_STRING));
-        headerList.removeAll(Arrays.asList("\r","","\n"));//delete empty because of while loop
+        }while (!list.get(j).equals(EMPTY_SIGN));
 
-        //что найдем в новом массиве, такую инфу в старый допишем
         if(!headerList.get(0).contains(HTTP)){
             output="Invalid response!";
             return output;
         }
-        parseHttp(headerList.get(0));
-        for (String line:headerList) {
-            System.out.println(line);
-        }
+        list.set(0,parseHttp(headerList.get(0)));
+        headerList.remove(0);
+        //todo тут потом придется в итый класть новый i+1
+
+        //todo что найдем в новом массиве, такую инфу в старый допишем
+//        for (String line:headerList) {
+//            System.out.println(line);
+//        }
+
+        //todo hash map is a good idea
+
         return output;
     }
 
-    private static void parseHttp(String input){
+    private static String parseHttp(String input){
         List<String> list = Arrays.asList(input.split(SPACE));
 
-        list.add(0,list.get(0)+" The protocol version that web server is using\n");
-        list.add(1,list.get(1)+" The http response status code\n");
-        list.add(2,list.get(2)+" The text version of status code\n");
+        list.set(0,list.get(0)+" The protocol version that web server is using\n");
+        list.set(1,list.get(1)+" The http response status code\n");
+        list.set(2,list.get(2)+" The text version of status code\n");
         StringBuilder sb = new StringBuilder();
-        for (String s:list){
-            sb.
+        for (String s: list){
+            sb.append(s);
         }
+        input=sb.toString();
+        return input;
     }
 }
--- WebSocket/web/index.jsp
+++ WebSocket/web/index.jsp
@@ -8,21 +8,20 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
-      <title>Title</title>
+      <title>Index</title>
       <link rel="stylesheet" type="text/css" href="css/style.css"/>
   </head>
   <body>
-
     <header>
         <h3>Lorem ipsum</h3>
     </header>
-
     <section>
       <article>
           <div>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </div>
       </article>
         <article>
@@ -32,9 +31,8 @@
             </form>
         </article>
     </section>
-
     <footer>
-        <p>&copy; 2016 Veronika. All rights reserved.</p>
+        <p>&copy; 2016 VeronikaChigir. All rights reserved.</p>
     </footer>
   </body>
 </html>
