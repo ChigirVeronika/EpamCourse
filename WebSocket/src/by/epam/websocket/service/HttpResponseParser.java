4cb5fc4f3f6f2566eaa113bef00d9c364a1c7800
ViolaWittrockiana <chigir.vera@mail.ru>
2016-01-27 14:25:25 +0300

    parsing

--- WebSocket/src/by/epam/websocket/service/HttpResponseParser.java
+++ WebSocket/src/by/epam/websocket/service/HttpResponseParser.java
@@ -3,14 +3,14 @@ package by.epam.websocket.service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Parse header of http response.
  */
 public class HttpResponseParser {
-    private static final String STATUS_CODE = "(\\s{1}\\d+)";
-    private static final String TEXT_CODE = "([a-zA-Z&&[^HTTP]])";
-    
+    private static final String HTTP_STRING = "(HTTP/\\d\\.\\d)\\s(\\d+)\\s([a-zA-Z&&[^HTTP]]+)";
 
     private static final String NEXT_LINE="\n";
     private static final String EMPTY_SIGN="\r";
@@ -34,31 +34,40 @@ public class HttpResponseParser {
             output="Invalid response!";
             return output;
         }
-        list.set(0,parseHttp(headerList.get(0)));
+
+        list.set(0,parseHttpString(headerList.get(0)));
         headerList.remove(0);
         //todo тут потом придется в итый класть новый i+1
 
         //todo что найдем в новом массиве, такую инфу в старый допишем
-//        for (String line:headerList) {
-//            System.out.println(line);
-//        }
+
 
         //todo hash map is a good idea
 
-        return output;
+        return output+list.get(0);
     }
 
-    private static String parseHttp(String input){
-        List<String> list = Arrays.asList(input.split(SPACE));
-
-        list.set(0,list.get(0)+" The protocol version that web server is using\n");
-        list.set(1,list.get(1)+" The http response status code\n");
-        list.set(2,list.get(2)+" The text version of status code\n");
+    private static String parseHttpString(String input){
         StringBuilder sb = new StringBuilder();
-        for (String s: list){
-            sb.append(s);
+        Pattern pattern = Pattern.compile(HTTP_STRING);
+        Matcher matcher=pattern.matcher(input);
+        while (matcher.find()){
+            if (matcher.group(1) != null) {
+                sb.append(matcher.group(1)+" The protocol version that web server is using\n");
+            }
+            if (matcher.group(2) != null) {
+                sb.append(matcher.group(2)+" The http response status code\n");
+            }
+            if (matcher.group(3) != null) {
+                sb.append(matcher.group(3)+" The text version of status code\n");
+            }
         }
-        input=sb.toString();
+        input = sb.toString();
+        return input;
+    }
+
+    private static String parseHeaderStrings(String input){
+
         return input;
     }
 }
